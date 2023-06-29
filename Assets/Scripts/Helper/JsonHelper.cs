47064f9de37602cd123bb5a56230dcaabdc164de
Dailin <3452720699@qq.com>
2023-06-29 16:23:41 +0800

    使用LitJSON更好的组织网络数据

---
+++ Assets/Scripts/Helper/JsonHelper.cs
@@ -0,0 +1,23 @@
+﻿using LitJson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class JsonHelper
+{
+    public static string ToJson(object x)
+    {
+        return JsonMapper.ToJson(x);
+    }
+    public static T ToObject<T>(string x)
+    {
+        return JsonMapper.ToObject<T>(x);
+    }
+    public static T ToObject<T>(byte[] bytes)
+    {
+        return ToObject<T>(Encoding.UTF8.GetString(bytes, 0, bytes.Length));
+    }
+}
+
